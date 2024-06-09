97b0587ea27c759cbb5bad2e1cae3ca38d5f81d1
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-06-09 10:06:31 +0000

    Create owner permissions, create post app

---
+++ posts/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
+    profile_id = ReadOnlyField(owner.profile.id)
+    profile_image = ReadOnlyField(owner.profile.image.url)
+
+    def get_is_owner(self, obj):
+        request = self.context["request"]
+        return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "owner",
+            "is_owner",
+            "profile_id",
+            "profile_image",
+            "created_at",
+            "updated_at",
+            "title",
+            "content",
+            "image",
+        ]
