af9e9d2c43b684a1d647a9560aab3cd950ba2a37
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-08-06 14:59:17 +0000

    Add Follower/Follwed counts to Profiles serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -41,5 +41,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter', 'like_id',
+            'title', 'content', 'image', 'image_filter',
+            'like_id',
         ]
