61f7c7313152f53846aea5673747371286a1514f
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-08-06 13:51:10 +0000

    Finish Likes app, add Followers app

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,19 +1,18 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from likes.models import Like
 
 
 class LikeSerializer(serializers.ModelSerializer):
     """
     Serializer for the Like model
+    The create method handles the unique constraint on 'owner' and 'post'
     """
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
-        model = Comment
-        fields = [
-            'id', 'owner','post', 'created_at', 
-        ]
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'post']
 
     def create(self, validated_data):
         try:
