5a96dbe93cd81755b822ba9ddd1dfd9a0c6302a5
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-08-07 14:28:58 +0000

    Changing settings to fix bad deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -59,10 +59,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST'),
-    'localhost',
-]
+ALLOWED_HOSTS = 'localhost', 'drf-gameshots-api-8431324dd3eb.herokuapp.com'
 
 
 # Application definition
